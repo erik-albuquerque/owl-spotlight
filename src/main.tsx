8da88059e4bcaa1dba34c8f4b91a8ae94ae098a3
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2024-11-26 13:28:18 -0300

    chore: setup biomejs

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import "./main.css";
+import './main.css'
 
-import { App } from "./app.tsx";
+import { App } from './app.tsx'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
 // Use contextBridge
-window.ipcRenderer.on("main-process-message", (_event, message) => {
-	console.log(message);
-});
+window.ipcRenderer.on('main-process-message', (_event, message) => {
+  console.log(message)
+})
